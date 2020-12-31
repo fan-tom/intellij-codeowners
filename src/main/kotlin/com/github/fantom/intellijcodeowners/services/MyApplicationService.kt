1d42f0f35d85ae299ce1386c8099396947179a86
GitHub Action <action@github.com>
2020-12-31 15:48:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/fantom/intellijcodeowners/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/fantom/intellijcodeowners/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.fantom.intellijcodeowners.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.fantom.intellijcodeowners.MyBundle
 
 class MyApplicationService {
 
