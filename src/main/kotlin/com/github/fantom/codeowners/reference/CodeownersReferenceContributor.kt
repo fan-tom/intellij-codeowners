0c57ce1a1e4713858d2fd29216e169eb2a63c61a
Alexey Gerasimov <lokomot476@gmail.com>
2021-02-08 22:25:26 +0500

    Add reference contributor

---
+++ src/main/kotlin/com/github/fantom/codeowners/reference/CodeownersReferenceContributor.kt
@@ -0,0 +1,32 @@
+package com.github.fantom.codeowners.reference
+
+import com.github.fantom.codeowners.language.psi.CodeownersEntry
+import com.github.fantom.codeowners.language.psi.CodeownersFile
+import com.intellij.patterns.PlatformPatterns
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiReference
+import com.intellij.psi.PsiReferenceContributor
+import com.intellij.psi.PsiReferenceProvider
+import com.intellij.psi.PsiReferenceRegistrar
+import com.intellij.util.ProcessingContext
+
+/**
+ * PSI elements references contributor.
+ */
+class CodeownersReferenceContributor : PsiReferenceContributor() {
+
+    override fun registerReferenceProviders(psiReferenceRegistrar: PsiReferenceRegistrar) {
+        psiReferenceRegistrar.registerReferenceProvider(
+                PlatformPatterns.psiElement().inFile(PlatformPatterns.psiFile(CodeownersFile::class.java)),
+                CodeownersReferenceProvider()
+        )
+    }
+
+    private class CodeownersReferenceProvider : PsiReferenceProvider() {
+        override fun getReferencesByElement(psiElement: PsiElement, processingContext: ProcessingContext): Array<out PsiReference> =
+                when (psiElement) {
+                    is CodeownersEntry -> CodeownersReferenceSet(psiElement).allReferences
+                    else -> PsiReference.EMPTY_ARRAY
+                }
+    }
+}
