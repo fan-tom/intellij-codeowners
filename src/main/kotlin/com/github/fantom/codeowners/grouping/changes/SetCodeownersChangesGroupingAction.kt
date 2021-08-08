70da1541030d54d5c2789e54f4d4d3909a94fe10
Alexey Gerasimov <lokomot476@gmail.com>
2021-08-08 22:37:56 +0500

    Move changes grouping by codeowner code to dedicated package

---
+++ src/main/kotlin/com/github/fantom/codeowners/grouping/changes/SetCodeownersChangesGroupingAction.kt
@@ -0,0 +1,16 @@
+package com.github.fantom.codeowners.grouping.changes
+
+import com.github.fantom.codeowners.CodeownersManager
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.components.service
+import com.intellij.openapi.vcs.changes.actions.SetChangesGroupingAction
+
+class SetCodeownersChangesGroupingAction : SetChangesGroupingAction() {
+    override val groupingKey: String get() = "codeowners"
+
+    override fun update(e: AnActionEvent) {
+        super.update(e)
+        val manager = e.project?.service<CodeownersManager>()
+        e.presentation.isEnabledAndVisible = e.presentation.isEnabledAndVisible && manager?.isAvailable ?: false
+    }
+}
