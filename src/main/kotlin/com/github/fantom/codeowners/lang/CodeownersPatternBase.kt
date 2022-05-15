d5dc54ba8e8d25859d5fad12733483bfaf158c15
Alexey Gerasimov <lokomot476@gmail.com>
2022-05-16 01:37:34 +0400

    Introduce language-agnostic CodeownersPatternBase interface to unify work with different languages

--- src/main/kotlin/com/github/fantom/codeowners/lang/CodeownersPatternBase.kt
+++ src/main/kotlin/com/github/fantom/codeowners/lang/CodeownersPatternBase.kt
@@ -1,29 +1,22 @@
 package com.github.fantom.codeowners.lang
-//
-// import com.intellij.psi.PsiElement
-// import java.util.regex.Pattern
-//
-// interface CodeownersEntryBase : PsiElement {
-//    /**
-//     * Returns current value.
-//     *
-//     * @return value
-//     */
-//    val value: String
-//
-//    val isDirectory: Boolean
-//
-//    /**
-//     * Returns current pattern.
-//     *
-//     * @return pattern
-//     */
-//    fun regex(acceptChildren: Boolean = false): String
-//
-//    /**
-//     * Returns current pattern.
-//     *
-//     * @return pattern
-//     */
-//    fun pattern(acceptChildren: Boolean = false): Pattern?
-// }
+
+import com.intellij.psi.PsiElement
+
+interface CodeownersPatternBase<EntryType : CodeownersEntryBase, OwnersType> : PsiElement {
+    /**
+     * Returns fs path.
+     */
+    val entry: EntryType
+
+    /**
+     * Returns non-empty list if this pattern assigns owners
+     * and empty list if it resets them (negation case)
+     */
+    val owners: List<OwnersType>
+
+    /**
+     * Some CODEOWNERS implementations allow to unset any early assigned ownership for given file path
+     */
+    val isExplicitlyUnowned: Boolean
+}
+
